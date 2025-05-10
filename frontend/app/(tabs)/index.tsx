eee73340e765ce050b61c63673545ade22f1e7e0
Shubhan Yenuganti <yrshubhan@gmail.com>
2025-05-10 14:48:39 -0700

    Complete frontend listings layout

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,30 +1,30 @@
-import { StyleSheet } from "react-native";
+// import { StyleSheet } from "react-native";
 
-import EditScreenInfo from "@/components/EditScreenInfo";
-import { Text, View } from "@/components/Themed";
-import ReviewScreen from "./review";
+// import EditScreenInfo from "@/components/EditScreenInfo";
+// import { Text, View } from "@/components/Themed";
+// import ReviewScreen from "../reviewComponents/review";
 
-export default function TabOneScreen() {
-  return (
-    <View style={styles.container}>
-      <ReviewScreen />
-    </View>
-  );
-}
+// export default function TabOneScreen() {
+//   return (
+//     <View style={styles.container}>
+//       <ReviewScreen />
+//     </View>
+//   );
+// }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+//   title: {
+//     fontSize: 20,
+//     fontWeight: "bold",
+//   },
+//   separator: {
+//     marginVertical: 30,
+//     height: 1,
+//     width: "80%",
+//   },
+// });

7a79e8a5e22d389eea1ec444f0add7145dd97d7f
phongtnguyen2006 <124423542+phongtnguyen2006@users.noreply.github.com>
2025-05-31 16:29:52 -0700

    added settings screen to profile

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -64,8 +64,8 @@ function RootLayoutNav() {
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack screenOptions={{ headerShown: false }}>
+        {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> */}
         <Stack.Screen name="modal" options={{ presentation: "modal" }} />
       </Stack>
     </ThemeProvider>
